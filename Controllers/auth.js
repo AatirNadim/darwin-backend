393eecea02f65905db3e58a6d57af5131744d023
Aatir Nadim <aatir.nadim@gmail.com>
2023-04-06 22:20:02 +0530

    middleware to verify token

--- Controllers/auth.js
+++ Controllers/auth.js
@@ -20,3 +20,23 @@ export const createRefreshToken = (user) => {
     }
   );
 };
+
+
+
+// this is the controller to verify the token when the user is logged in.
+
+export const verifyToken = async (req, res, next)  => {
+  const token = req.body.token;
+  if(!token) return res.status(403).json({
+    message : 'A token is required for authentication'
+  })
+  try {
+    const decode = jwt.verify(token, 'access');
+    req.decoded = decode;
+  } catch(err) {
+    return res.status(401).json({
+      message : 'Invalid token'
+    })
+  }
+  return next();
+}
